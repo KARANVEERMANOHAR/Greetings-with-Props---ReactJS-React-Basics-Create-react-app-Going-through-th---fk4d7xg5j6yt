4335dfa4ab5c4fa8c693e234d629a375f14d45c9
onkar-kale-ok <96816723+onkar-kale-ok@users.noreply.github.com>
2022-08-26 21:28:19 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,10 +4,10 @@ function Welcome(props) {
     const name = props.name;
     // console.log(name);
   return (
-    <div>
+    <>
       <h1>Hey ! {name}</h1>
       <h2>Welcome to Newton School.</h2>
-    </div>
+    </>
   )
 }
 
