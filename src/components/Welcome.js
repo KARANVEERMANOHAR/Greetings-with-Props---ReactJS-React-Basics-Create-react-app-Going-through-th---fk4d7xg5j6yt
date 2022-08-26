5c59d50a291f71d2f8c18b64fc95a998048a6aa6
onkar-kale-ok <96816723+onkar-kale-ok@users.noreply.github.com>
2022-08-26 21:21:27 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,14 @@
-// write code for Welcome component here
+import React from 'react'
+
+function Welcome(props) {
+    const name = props.name;
+    // console.log(name);
+  return (
+    <div>
+      <h1>Hey ! {name}</h1>
+      <h2>Welcome to Newton School.</h2>
+    </div>
+  )
+}
+
+export default Welcome
